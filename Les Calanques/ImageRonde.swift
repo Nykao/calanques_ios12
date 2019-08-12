5a912fc7cc0b37560b177e2975a2efacbc38a909
Nykao <nickaoo@hotmail.fr>
2019-08-12 15:29:26 +0200

    UITableView Delegate & DataSource

---
+++ Les Calanques/ImageRonde.swift
@@ -0,0 +1,37 @@
+//
+//  ImageRonde.swift
+//  Les Calanques
+//
+//  Created by Nicolas Meunier on 12/08/2019.
+//  Copyright © 2019 Nicolas Meunier. All rights reserved.
+//
+
+import UIKit
+
+class ImageRonde: UIImageView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setup()
+    }
+    
+    func setup() {
+        layer.cornerRadius = frame.height / 2
+        layer.borderColor = UIColor.white.cgColor
+        layer.borderWidth = 2
+        clipsToBounds = true
+        contentMode = .scaleAspectFill
+    }
+}
